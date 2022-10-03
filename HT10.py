83dc708b578505e537eb22cc07e5f80eaa1a00ea
Regina <regina.delmukhametova@gmail.com>
2022-10-03 22:58:03 +0500

    Homework 2. Task 10, realize algorithm of mixing list

---
+++ HT10.py
@@ -0,0 +1,8 @@
+# Реализуйте алгоритм перемешивания списка
+import random
+
+my_list = [1,2,3,4,5,6,7]
+new_list = []
+for i in range(len(my_list)):
+    new_list.append(random.choice(my_list))
+print(new_list)
