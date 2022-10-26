30ba7279f88fea437df13ace639ec32ff2939c46
Regina <regina.delmukhametova@gmail.com>
2022-10-26 13:12:52 +0500

    Delete wrong file

--- HT10.py
+++
@@ -1,8 +0,0 @@
-# Реализуйте алгоритм перемешивания списка
-import random
-
-my_list = [1,2,3,4,5,6,7]
-new_list = []
-for i in range(len(my_list)):
-    new_list.append(random.choice(my_list))
-print(new_list)
